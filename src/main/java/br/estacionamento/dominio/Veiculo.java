f646e6b87e14ef0d5b45274967db33ef9e23bf46
Tarley Lana <tarley.lana@gmail.com>
2023-10-28 12:59:31 -0300

    feat: Implementa métodos de registro de entrada/saída e getHoras/getValor na classe Ticket
    Nesta aula de 27/10, adicionamos os métodos de registro de entrada/saída, bem como otimizamos o código de getValor. No entanto, o getValor agora inclui aninhamentos complexos de condicionais. Qualquer alteração nessa lógica exigirá testes abrangentes em todas as opções de cálculo de estacionamento.

--- src/main/java/br/estacionamento/dominio/Veiculo.java
+++ src/main/java/br/estacionamento/dominio/Veiculo.java
@@ -1,5 +1,42 @@
 package br.estacionamento.dominio;
 
+import java.util.Objects;
+
 public class Veiculo {
     private String placa;
+
+    public Veiculo() {
+    }
+
+    public Veiculo(String placa) {
+        this.placa = placa;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Veiculo veiculo = (Veiculo) o;
+        return Objects.equals(placa, veiculo.placa);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(placa);
+    }
+
+    @Override
+    public String toString() {
+        return "Veiculo{" +
+                "placa='" + placa + '\'' +
+                '}';
+    }
 }
